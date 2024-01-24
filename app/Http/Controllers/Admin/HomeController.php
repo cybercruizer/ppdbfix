e2170cb444f03e5cf28d1f03e4bb6d282ef09240
cybercruizer <cybercruizer@gmail.com>
2024-01-24 23:37:39 +0700

    Perbaikan tampilan dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Models\Siswa;
+use Illuminate\Http\Request;
+use App\Models\Bendahara\Payment;
+use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
@@ -25,7 +26,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $siswa= Siswa::all();
+        $siswa= Siswa::get();
+        $siswadu=Siswa::has('payments')->count();
         $tkj = $siswa->where('jurusan','TKJ')->count();
         $tsm = $siswa->where('jurusan','TSM')->count();
         $tkr = $siswa->where('jurusan','TKR')->count();
@@ -33,13 +35,14 @@ class HomeController extends Controller
         $tpm = $siswa->where('jurusan','TPM')->count();
         $kuliner = $siswa->where('jurusan','KUL')->count();
         $perhotelan = $siswa->where('jurusan','PHT')->count();
-        $tkjdu = $siswa->where('du','1')->where('jurusan','TKJ')->count();
-        $tsmdu = $siswa->where('du','1')->where('jurusan','TSM')->count();
-        $tkrdu = $siswa->where('du','1')->where('jurusan','TKR')->count();
-        $titldu = $siswa->where('du','1')->where('jurusan','TITL')->count();
-        $tpmdu = $siswa->where('du','1')->where('jurusan','TPM')->count();
-        $kulinerdu = $siswa->where('du','1')->where('jurusan','KUL')->count();
-        $perhotelandu = $siswa->where('du','1')->where('jurusan','PHT')->count();
+
+        $tkjdu = Siswa::where('jurusan','TKJ')->whereHas('payments')->count();
+        $tsmdu = Siswa::where('jurusan','TSM')->whereHas('payments')->count();
+        $tkrdu = Siswa::where('jurusan','TKR')->whereHas('payments')->count();
+        $titldu = Siswa::where('jurusan','TITL')->whereHas('payments')->count();
+        $tpmdu = Siswa::where('jurusan','TPM')->whereHas('payments')->count();
+        $kulinerdu = Siswa::where('jurusan','KUL')->whereHas('payments')->count();
+        $perhotelandu = Siswa::where('jurusan','PHT')->whereHas('payments')->count();
 
         $data = [
             ['label' => 'TKJ', 'value' => $tkj, 'sub_value' => $tkjdu],
@@ -51,6 +54,6 @@ class HomeController extends Controller
             ['label' => 'PHT', 'value' => $perhotelan, 'sub_value' => $perhotelandu],
         ];
         //dd($data);
-        return view('home', compact(['siswa','data']));
+        return view('home', compact(['siswa','siswadu','data']));
     }
 }
